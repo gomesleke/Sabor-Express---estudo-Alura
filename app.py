d4e746cd430d08f9d58e4a9f78898d7e2a787d87
gomesleke <voyagerleke@gmail.com>
2025-12-20 17:05:10 -0300

    add um sistema try, ele nao quebra o opcao_escolha caso algm colocar uma letra ao inves de um numero

--- app.py
+++ app.py
@@ -23,25 +23,34 @@ def exit_app(): #função de saida
         time.sleep(1)
         repete+=1
 
+def invalid():
+    print('Opção Inválida\n')
+    input('Digite uma tecla para voltar ao menu: ')
+    main()
+
 def escolha_opcao():
-    opcao_escolha=int(input('Escolha uma opção: ')) #sei_la -> snake case (variavel e afins)
+    try:
+        opcao_escolha=int(input('Escolha uma opção: ')) #sei_la -> snake case (variavel e afins)
 
 
-    #bool na escolha
-    match opcao_escolha:
+        #bool na escolha
+        match opcao_escolha:
 
-        case 1: 
-            print('Cadrastrar Restaurante')
-        case 2:
-            print('Listar Restaurantes')
-        case 3: 
-            print('Ativar Restaurantes')
-        case 4:
-            exit_app()
-        case _:
-            print("Inválido")
+            case 1: 
+                print('Cadrastrar Restaurante')
+            case 2:
+                print('Listar Restaurantes')
+            case 3: 
+                print('Ativar Restaurantes')
+            case 4:
+                exit_app()
+            case _:
+                invalid()
+    except:
+        invalid()
 
 def main(): #controla ordem do projeto (para manutenção)
+    os.system('cls')
     name_program()
     options()
     escolha_opcao()
