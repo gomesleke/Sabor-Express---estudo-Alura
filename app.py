1a31604feba34f3f91673669e4321484e1674789
gomesleke <voyagerleke@gmail.com>
2025-12-20 16:48:31 -0300

    add funções no main e tornei app.py como principal

--- app.py
+++ app.py
@@ -3,19 +3,16 @@
 import time
 import os
 
-print("""
+def name_program():
+    print("""
 █▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
 ▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█\n""")
 
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurantes')
-print('3. Ativar Restaurantes')
-print('4. Sair\n')
-
-opcao_escolha=int(input('Escolha uma opção: ')) #sei_la -> snake case (variavel e afins)
-
-
-#bool na escolha
+def options():  
+    print('1. Cadastrar Restaurante')
+    print('2. Listar Restaurantes')
+    print('3. Ativar Restaurantes')
+    print('4. Sair\n')
 
 def exit_app(): #função de saida
     repete=0
@@ -26,11 +23,27 @@ def exit_app(): #função de saida
         time.sleep(1)
         repete+=1
 
-if opcao_escolha==1:
-    print('Cadrastrar Restaurante')
-elif opcao_escolha==2:
-    print('Listar Restaurantes')
-elif opcao_escolha==3:
-    print('Ativar Restaurantes')
-else:
-    exit_app()
+def escolha_opcao():
+    opcao_escolha=int(input('Escolha uma opção: ')) #sei_la -> snake case (variavel e afins)
+
+
+    #bool na escolha
+
+
+    if opcao_escolha==1:
+        print('Cadrastrar Restaurante')
+    elif opcao_escolha==2:
+        print('Listar Restaurantes')
+    elif opcao_escolha==3:
+        print('Ativar Restaurantes')
+    else:
+        exit_app()
+
+def main(): #controla ordem do projeto (para manutenção)
+    name_program()
+    options()
+    escolha_opcao()
+
+
+if __name__=='__main__':  
+    main()
